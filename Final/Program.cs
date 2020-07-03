b3a975a7df46260fde986653ca3883e7ab324fc2
Gurwinder Kaur <gurwinderkaur.ca@gamil.com>
2020-07-02 20:45:55 -0500

    My Final Test

---
+++ Final/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Final
+{
+    public class Adult
+    {
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+
+
+    }
+    public class Child : Adult
+    {
+        public Adult a { get; set; }
+        public Child c { get; set; }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<AdultChild> list = new IList<AdultChild>;
+            Adult adult = new Adult();
+            adult.FirstName = "Abc";
+            adult.lastname = "xyz";
+            list.Add(adult);
+            Child child = new Child();
+            child.FirstName = "www";
+            child.lastname = "yyy";
+            list.Add(child);
+        }
+    }
+
+}
+}
